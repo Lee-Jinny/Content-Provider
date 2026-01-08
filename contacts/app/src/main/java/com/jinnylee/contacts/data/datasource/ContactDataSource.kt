3b8ad7f0df86ec80c8b86df8d6f9b234ef85b40d
Jieun Lee <geniee1750@gmail.com>
2026-01-08 17:01:39 +0900

    [과제] feat: 연습문제 2 시스템 연락처 Data Source

---
+++ contacts/app/src/main/java/com/jinnylee/contacts/data/datasource/ContactDataSource.kt
@@ -0,0 +1,41 @@
+package com.jinnylee.contacts.data.datasource
+
+import android.content.ContentResolver
+import android.provider.ContactsContract
+import com.jinnylee.contacts.domain.model.Contact
+
+class ContactsDataSource(
+    private val contentResolver: ContentResolver
+) {
+
+    fun getContacts(): List<Contact> {
+        val result = mutableListOf<Contact>()
+
+        val cursor = contentResolver.query(
+            ContactsContract.CommonDataKinds.Phone.CONTENT_URI,
+            null,
+            null,
+            null,
+            null
+        )
+
+        cursor?.use {
+            val idIdx = it.getColumnIndex(ContactsContract.CommonDataKinds.Phone.CONTACT_ID)
+            val nameIdx = it.getColumnIndex(ContactsContract.CommonDataKinds.Phone.DISPLAY_NAME)
+            val numberIdx = it.getColumnIndex(ContactsContract.CommonDataKinds.Phone.NUMBER)
+
+            while (it.moveToNext()) {
+                val name = it.getString(nameIdx)
+                result.add(
+                    Contact(
+                        id = it.getLong(idIdx),
+                        name = name,
+                        phoneNumber = it.getString(numberIdx),
+                        initial = name.first().toString()
+                    )
+                )
+            }
+        }
+        return result
+    }
+}
